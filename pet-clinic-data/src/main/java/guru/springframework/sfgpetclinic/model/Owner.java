d9200f23aedd0834f5849b8c8170daecc9cc324e
i-sokar <simosokar5@gmail.com>
2023-06-16 08:10:34 +0300

    added @id +@table&@column

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,12 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
+    @OneToMany(mappedBy = "owner",cascade = CascadeType.ALL)
     private Set<Pet> pets=new HashSet<>();
+    @Column(name ="city" )
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+    @Column(name = "address")
     private String address;
 
     public Set<Pet> getPets() {
